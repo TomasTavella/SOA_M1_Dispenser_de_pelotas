9322aef7c79a666225878d8a9c479c1fdab87122
TomasTavella <tomytavella@gmail.com>
2024-04-20 21:11:46 -0300

    implementación de maquina de estados y ejemplo

---
+++ codigo/dispenser_de_pelotas.cpp
@@ -0,0 +1,271 @@
+// ------------------------------------------------
+// Etiquetas
+// ------------------------------------------------
+#define LOG // Comentar esta linea para desactivar logs
+
+
+// ------------------------------------------------
+// Constantes
+// ------------------------------------------------
+#define VALUE_CONTINUE -1
+
+
+// ------------------------------------------------
+// states del embebido
+// ------------------------------------------------
+enum state_e
+{
+    STATE_CHECKING,
+    STATE_READY,
+    STATE_EMPTY,
+    STATE_DOG_DETECTED,
+    STATE_DROP_BALL,
+    STATE_END_OF_SERVICE    //agrego estado para final de servicio (espera a que el perro se aleje)
+};
+
+// ------------------------------------------------
+// events posibles
+// ------------------------------------------------
+enum event_e
+{
+    EVENT_NOT_EMPTY,
+    EVENT_EMPTY,
+    EVENT_DOG_NEARBY,
+    EVENT_BUTTON,
+    EVENT_TIMEOUT_WAIT,
+    EVENT_TIMEOUT_CLOSE_SERVO,
+    EVENT_DOG_AWAY,     //agrego evento para detectar cuando se aleja el perro (para que no siga tirando pelotas)
+    EVENT_CONTINUE
+};
+
+
+// ------------------------------------------------
+// Estructura de event
+// ------------------------------------------------
+typedef struct event_s
+{
+    event_e type;
+    int value;
+} event_t;
+
+
+// ------------------------------------------------
+// Variables globales
+// ------------------------------------------------
+state_e actual_state;
+event_t event;
+
+
+// ------------------------------------------------
+// Logs
+// ------------------------------------------------
+void log(const char *state, const char *event)
+{
+#ifdef LOG
+    Serial.println("------------------------------------------------");
+    Serial.println(state);
+    Serial.println(event);
+    Serial.println("------------------------------------------------");
+#endif
+}
+
+void log(const char *msg)
+{
+#ifdef LOG
+    Serial.println(msg);
+#endif
+}
+
+void log(int val)
+{
+#ifdef LOG
+    Serial.println(val);
+#endif
+}
+// ------------------------------------------------
+// Captura de eventos
+// ------------------------------------------------
+void catch_event()
+{
+
+}
+
+// ------------------------------------------------
+// Inicialización
+// ------------------------------------------------
+void start()
+{
+
+}
+
+// ------------------------------------------------
+// Implementación maquina de estados
+// ------------------------------------------------
+void fsm()
+{
+
+    catch_event();
+
+    switch (actual_state)
+    {
+    case STATE_CHECKING:
+        switch (event.type)
+        {
+        case EVENT_NOT_EMPTY:
+            //Activar actuadores
+            //LED VERDE
+            log("STATE_CHECKING", "EVENT_NOT_EMPTY");
+            state_actual = STATE_READY;
+            break;
+
+        case EVENT_EMPTY:
+            //Activar actuadores
+            //LED ROJO
+            log("STATE_CHECKING", "EVENT_EMPTY");
+            //state_actual = STATE_EMPTY;       //este no iría si lo hacemos sin estado EMPTY
+            state_actual = STATE_CHECKING; //este iría si lo hacemos sin estado EMPTY
+            break;
+
+        case EVENT_CONTINUE:
+            log("STATE_CHECKING", "EVENT_CONTINUE");
+            state_actual = STATE_CHECKING;
+            break;
+
+        default:
+            break;
+        }
+        break;
+
+        //                      Este estado no iría en caso de manejar el evento EMPTY en el estado CHECKING
+        // case STATE_EMPTY:
+        //     switch (event.type)
+        //     {
+        //     case EVENT_NOT_EMPTY:
+        //         //Activar actuadores
+        //         //LED VERDE
+        //         log("STATE_EMPTY", "EVENT_NOT_EMPTY");
+        //         state_actual = STATE_READY;
+        //         break;
+        //     case EVENT_CONTINUE:
+        //         log("STATE_EMPTY", "EVENT_CONTINUE");
+        //         state_actual = STATE_EMPTY;
+        //         break;
+        //     }
+        //     break;
+
+    case STATE_READY:
+        switch (event.type)
+        {
+        case EVENT_DOG_NEARBY:
+            //Activar actuadores
+            //LED AMARILLO
+            log("STATE_READY", "EVENT_DOG_NEARBY");
+            state_actual = STATE_DOG_DETECTED;
+            break;
+
+        case EVENT_BUTTON:
+            //Activar actuadores
+            //LED AMARILLO
+            //SERVIR PELOTA
+            log("STATE_READY", "EVENT_BUTTON");
+            state_actual = STATE_DROP_BALL;
+            break;
+
+        case EVENT_CONTINUE:
+            log("STATE_READY", "EVENT_CONTINUE");
+            state_actual = STATE_READY;
+            break;
+
+        default:
+            break;
+        }
+        break;
+
+    case STATE_DOG_DETECTED:
+        switch (event.type)
+        {
+        case EVENT_TIMEOUT_WAIT:
+            //Activar actuadores
+            //SERVIR PELOTA
+            log("STATE_DOG_DETECTED", "EVENT_TIME_OUT_WAIT");
+            state_actual = STATE_DROP_BALL;
+            break;
+
+        case EVENT_BUTTON:
+            //Activar actuadores
+            //SERVIR PELOTA
+            log("STATE_DOG_DETECTED", "EVENT_BUTTON");
+            state_actual = STATE_DROP_BALL;
+            break;
+
+        case EVENT_CONTINUE:
+            log("STATE_DOG_DETECTED", "EVENT_CONTINUE");
+            state_actual = STATE_DOG_DETECTED;
+            break;
+
+        default:
+            break;
+        }
+
+    case STATE_DROP_BALL:
+        switch (event.type)
+        {
+        case EVENT_TIMEOUT_CLOSE_SERVO:
+            //Activar actuadores
+            //CERRAR SERVO
+            //APAGAR LED
+            log("STATE_DROP_BALL", "EVENT_TIMEOUT_CLOSE_SERVO");
+            state_actual = STATE_END_OF_SERVICE;
+            break;
+
+        case EVENT_CONTINUE:
+            log("STATE_DROP_BALL", "EVENT_CONTINUE");
+            state_actual = STATE_DROP_BALL;
+            break;
+
+        default:
+            break;
+        }
+
+    case STATE_END_OF_SERVICE:
+        switch (event.type)
+        {
+        case EVENT_DOG_AWAY:
+            //Activar actuadores
+
+            log("STATE_END_OF_SERVICE", "EVENT_DOG_AWAY");
+            state_actual = STATE_CHECKING;
+            break;
+
+        case EVENT_CONTINUE:
+            log("STATE_END_OF_SERVICE", "EVENT_CONTINUE");
+            state_actual = STATE_END_OF_SERVICE;
+            break;
+            
+        default:
+            break;
+        }
+        
+        break;
+    }
+
+    // Ya se atendió el event
+    event.type = EVENT_CONTINUE;
+    event.value = VALUE_CONTINUE;
+}
+
+// ------------------------------------------------
+// Arduino setup
+// ------------------------------------------------
+void setup()
+{
+    start();
+}
+
+// ------------------------------------------------
+// Arduino loop
+// ------------------------------------------------
+void loop()
+{
+    fsm();
+}
